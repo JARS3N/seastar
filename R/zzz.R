ab79acdf2c25d31cbd2edce234beca753c9a50d6
JARS3N <JARS3N@users.noreply.github.com>
2017-10-05 16:03:21 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -30,7 +30,7 @@ autoUpGithub<-function(pack){
   if (testUrl(GETS)==FALSE){return(message("Can't seem to contact github repo\n will not update."))}
   ongit<- gsub("Version: ","",grep("Version: ",readLines(GETS,warn=F),value=T))
   if (ongit!=utils::packageVersion(pack)){
-    message(paste0("Github version differs from installed version \n update",pack))
+    message(paste0("Github version differs from installed version \n update ",pack))
     devtools::install_github(gsub("https://github.com/","",pkg),quite=TRUE,dependencies = T,quick=T)
   }else{message(paste0("Github version is identical to installed \n no update for ",pack))}
 }
